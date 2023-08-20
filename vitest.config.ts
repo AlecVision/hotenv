7c2e81bd1d484ec67e971c8b5ff5a09d9a9a4a8d
Alec Helmturner <alec@helmturner.dev>
2023-08-19 21:39:08 -0500

    test: add console output test test: update thresholds ci: update scripts to use pnpm

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,13 +5,18 @@ export default defineConfig({
     environment: "node",
     include: ["**/*.ts"],
     coverage: {
-      "100": true,
+      thresholdAutoUpdate: true,
+      statements: 89.44,
+      branches: 90,
+      functions: 66.66,
+      lines: 89.44,
       reporter: ["html", "html-spa", "json-summary"],
       provider: "v8",
       reportsDirectory: "coverage",
       reportOnFailure: true,
     },
     typecheck: {
+      tsconfig: "tsconfig.json",
       checker: "tsc",
       include: ["src/**/*"]
     }
